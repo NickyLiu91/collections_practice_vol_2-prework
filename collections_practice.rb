4538d12cc2153b3ad3ea3e24f267858579809be5
Nicky Liu <nickyliu91@gmail.com>
2018-08-23 09:58:48 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -86,7 +86,7 @@ def organize_schools(array)
   array.each do |hash|
     puts hash
     hash.each do |school, hash2|
-      
+
 
       hash2.each do |key, location|
         array2 << [school, location]
