a953cf8e8e3289483d7d861c9b7aedf57ff1b2fb
Nicky Liu <nickyliu91@gmail.com>
2018-08-23 08:33:00 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -34,15 +34,15 @@ def count_elements(array)
   current_name = ""
   count = 0
   array2.sort.each do |name|
-    if current_name == "" || current_name == name
-      # count += 1
-      puts current_name
-    else
+    # if current_name == "" || current_name == name
+    #   # count += 1
+    #   puts current_name
+    # else
       # array3 << {:name => current_name, :count => count}
       # current_name = name
       # count = 1
       puts current_name
-    end
+    # end
   end
   # array3 << {:name => current_name, :count => count}
   puts array3
